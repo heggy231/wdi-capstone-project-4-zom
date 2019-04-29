4244b895db67c285909e78facfe0dfb9f8870a68
Heggyhere <heggyy@gmail.com>
2019-04-28 20:12:30 -0700

    update

--- models.py
+++ models.py
@@ -6,9 +6,9 @@ from peewee import * # peewee is a small and simple ORM, like mongoose but simpl
 from flask_login import UserMixin
 from flask_bcrypt import generate_password_hash
 
-DATABASE = SqliteDatabase('zom.db') 
+# DATABASE = SqliteDatabase('zom.db') 
 # DATABASE = PostgresqlDatabase('zom')
-# DATABASE = connect(os.environ.get('DATABASE_URL'))
+DATABASE = connect(os.environ.get('DATABASE_URL'))
 
 # We use the built in SqliteDatabase() function from peewee to save a reference to a DB file to a DATABASE variable
 # DATABASE = PostgresqlDatabase('zom')
